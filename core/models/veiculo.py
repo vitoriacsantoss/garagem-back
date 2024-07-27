1e01f4ffc5d74ed966dfb9aaca1e8221eea68f98
vitoriacsantoss <santosviih360@gmail.com>
2024-07-26 21:40:10 -0300

    Modificando veiculo model

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -3,12 +3,12 @@ from core.models import Modelo, Cor, Acessorio
 from uploader.models import Image
 
 class Veiculo(models.Model):
-    foto_veiculo = models.ForeignKey(Modelo, on_delete=models.CASCADE, related_name="veiculos")
+    modelo = models.ForeignKey(Modelo, on_delete=models.CASCADE, related_name="veiculos")
     cor = models.ForeignKey(Cor, on_delete=models.CASCADE, related_name="veiculos")
     ano = models.IntegerField(null=True, default=0)
     preco = models.DecimalField(max_digits=10, decimal_places=2, null=True, default=0)
     acessorios = models.ManyToManyField(Acessorio, related_name="veiculos")
-    imagem = models.ForeignKey(
+    foto_veiculo = models.ForeignKey(
         Image,
         related_name="+",
         on_delete=models.CASCADE,
