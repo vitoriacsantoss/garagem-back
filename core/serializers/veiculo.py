e0c936bca88e38f30e99b5347cea9c7b06b12116
vitoriacsantoss <santosviih360@gmail.com>
2024-07-26 21:38:10 -0300

    Modificando serializer do veiculo

--- core/serializers/veiculo.py
+++ core/serializers/veiculo.py
@@ -1,13 +1,32 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 from core.models import Veiculo
 
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
+
 class VeiculoDetailSerializer(ModelSerializer):
+    foto_veiculo_attachment_key = SlugRelatedField(
+        source="foto_veiculo",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    foto_veiculo = ImageSerializer(required=False, read_only=True)
     class Meta:
         model = Veiculo
         fields = "__all__"
         depth = 2
 
 class VeiculoSerializer(ModelSerializer):
+    foto_veiculo_attachment_key = SlugRelatedField(
+        source="foto_veiculo",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    foto_veiculo = ImageSerializer(required=False, read_only=True)
     class Meta:
         model = Veiculo
         fields = "__all__"
